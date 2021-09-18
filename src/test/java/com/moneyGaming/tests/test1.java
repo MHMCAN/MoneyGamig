514ebec9c03fd06475aa5d3b20ee506988c4766e
MHMCAN <mmrcan@gmail.com>
2021-09-18 18:07:46 +0300

    I added WebDriverFactory to utility class

--- src/test/java/com/moneyGaming/tests/test1.java
+++ src/test/java/com/moneyGaming/tests/test1.java
@@ -1,4 +1,28 @@
 package com.moneyGaming.tests;
 
+import com.moneyGaming.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class test1 {
+    @BeforeMethod
+    public  void setup(){
+
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test(){
+
+    }
+
+    @AfterMethod
+
+    public void tearDown(){
+
+    }
 }
---
+++ src/test/java/com/moneyGaming/utilities/WebDriverFactory.java
@@ -0,0 +1,40 @@
+package com.moneyGaming.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Locale;
+
+public class WebDriverFactory {
+    // task:
+    // write a static method that takes a string parameter name: browserType
+    // based on the value of parameter
+    // it will setup the browse and
+    // the method will return chromedriver or firefoxdriver object
+    // name of the method: getDriver
+
+   public static WebDriver getDriver(String browserType){
+
+      WebDriver driver=null;
+
+      switch(browserType.toLowerCase(Locale.ROOT)){
+
+         case "chrome":
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+            break;
+
+         case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            driver=new FirefoxDriver();
+            break;
+
+
+      }
+
+
+      return driver;
+   }
+}
